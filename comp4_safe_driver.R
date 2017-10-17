fe82d1c5724829dcdeaa189324b10ceb1dbb5bd2
Jo Pan <pan.huitong@yahoo.com>
2017-10-17 09:56:06 -0400

    r and first

---
+++ comp4_safe_driver.R
@@ -0,0 +1,15 @@
+#competition 4 safe driver
+#https://www.kaggle.com/c/porto-seguro-safe-driver-prediction/data
+
+train<-read.csv("train.csv")
+summary(train)
+test<-read.csv("test.csv")
+
+lmraw<-lm(target~.,data=train)
+
+mypred<-predict(lmraw,newdata = test)
+mypred[mypred<0]<-0
+mypred[mypred>1]<-1
+final_table<-data.frame(test$id, mypred)
+# Write files
+write.table(final_table, file="first.csv", row.names=F, col.names=c("id", "target"), sep=",")
