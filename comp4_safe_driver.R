17b98583c04e52065074b7e249580ab1c749e464
Jo Pan <pan.huitong@yahoo.com>
2017-10-25 23:29:54 -0400

    data cleaned & assigned tasks

--- comp4_safe_driver.R
+++ comp4_safe_driver.R
@@ -1,23 +1,41 @@
 #competition 4 safe driver
 #https://www.kaggle.com/c/porto-seguro-safe-driver-prediction/data
-
+#https://github.com/Jo-Pan/sys6018-competition-safe-driver
 train<-read.csv("train.csv")
-summary(train)
 test<-read.csv("test 2.csv")
-
-cat("Combine train and test files")
+train_id<-train$id
+#Combine train and test files
 test$target <- NA
 comb <- rbind(train, test)
 
-################  Data Cleaning  ##################
-# Set missing values to NA 
-train[train==-1]<-NA
+################  Data Cleaning  ################## (Jo)
+#indicate nas:
+comb[comb==-1]<-NA
 
-lmraw<-lm(target~.,data=train)
+#what % in columns are na:
+colMeans(is.na(comb)) #none is significantly large
 
+# collect names of all categorical variables
+cat_vars <- names(comb)[grepl('_cat$', names(comb))]
+comb[cat_vars]<-lapply(comb[cat_vars],factor)
+
+#split train & test sets
+train<-comb[train_id,]
+test<-comb[-train_id,]
+
+################  Linear Model  ################## (Teresa)
+### Basic Model ### (Jo)(can be deleted/commented out)
 mypred<-predict(lmraw,newdata = test)
 mypred[mypred<0]<-0
 mypred[mypred>1]<-1
 final_table<-data.frame(test$id, mypred)
 # Write files
 write.table(final_table, file="first.csv", row.names=F, col.names=c("id", "target"), sep=",")
+
+####################  K-NN  ##################### (Jo)
+
+############  Step-wise Regression  ############# (Jo)
+
+####################  Trees  #################### (Sai)
+
+################  Random Forest  ################ (Sai)
